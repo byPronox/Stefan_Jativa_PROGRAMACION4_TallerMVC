afee4ecc2c46bfe0b1c88fe9cda3f315bf063d56
byPronox <bypronoxyt@gmail.com>
2024-10-23 19:19:44 -0500

    Commit 2

---
+++ Migrations/20241023234928_Migracion1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stefan_Jativa_PROGRAMACION4_TallerMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracion1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estadio",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Direccion = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Ciudad = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Capacidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estadio", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Ciudad = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Titutlos = table.Column<int>(type: "int", nullable: false),
+                    AceptaExtranjeros = table.Column<bool>(type: "bit", nullable: false),
+                    IDestadio = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Equipo_Estadio_IDestadio",
+                        column: x => x.IDestadio,
+                        principalTable: "Estadio",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jugador",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Posicion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    IdEquipo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jugador", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jugador_Equipo_IdEquipo",
+                        column: x => x.IdEquipo,
+                        principalTable: "Equipo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipo_IDestadio",
+                table: "Equipo",
+                column: "IDestadio");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jugador_IdEquipo",
+                table: "Jugador",
+                column: "IdEquipo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jugador");
+
+            migrationBuilder.DropTable(
+                name: "Equipo");
+
+            migrationBuilder.DropTable(
+                name: "Estadio");
+        }
+    }
+}
